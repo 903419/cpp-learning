c47629cb332ac1f4737893db59b2bcc3684f4fc8
wei <987404656@qq.com>
2020-10-25 21:53:50 +0800

    友元

--- class/class/main.cpp
+++ class/class/main.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
-#include <cassert>
-#pragma warning(disable:4996)
-using namespace std;
+#include "main.h"
+
 
 /*
 class Time
@@ -59,7 +57,7 @@ void TestClass()
 }
 */
 
-
+/*
 class Date
 {
 public:
@@ -220,19 +218,113 @@ private:
 	int _day;
 };
 
+*/
+
+/*
+class Time
+{
+public:
+	Time(int hour, int mintue=10, int second=10)
+		:_hour(hour)
+		,_minute(mintue)
+		,_second(second)
+	{}
+private:
+	int _hour;
+	int _minute;
+	int _second;
+};
+
+
+
+class Date
+{
+	friend const void Print(Date& d);//友元函数
+	friend ostream&  operator<<(ostream& _cout, Date& d);
+public:
+	Date(int year, int month, int day)//构造函数
+		: _year(year)
+		, _month(month)
+		, _day(day)
+	{}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+ostream& operator<<(ostream& _cout,Date& d)
+{
+	_cout << d._year << "-" << d._month << "-" << d._day;
+	return _cout;
+}
+*/
+
+
+
+class Time
+{
+	friend class Date;
+public:
+	Time(int hour = 20, int mintue = 10, int second = 10)
+		:_hour(hour)
+		, _minute(mintue)
+		, _second(second)
+	{}
+	void Print()
+	{
+		cout << _hour << "-" << _minute << "-" << _second << endl;
+	}
+private:
+	int _hour;
+	int _minute;
+	int _second;
+};
+
+class Date
+{
+public:
+	Date(int year, int month, int day)//构造函数
+		: _year(year)
+		, _month(month)
+		, _day(day)
+	{}
+	void SetTime(int hour, int minute, int second)
+	{
+		_t._hour = hour;
+		_t._minute = minute;
+		_t._second = second;
+	}
+	void Print()
+	{
+		_t.Print();
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+	Time _t;
+};
+
+
 void TestClass()
 {
-	Date d1;
-	Date d2(2020,10,6);
-	Date d3(2020, 10, 1);
-	(d1 = d2) = d3;
+	Date d1(2020,10,6);
+	Date d2(2020, 10, 1);
+	d1.SetTime(21, 13, 5);
+	d1.Print();
+}
+
+
+void Func()
+{
+	static int a = 10;
+	a++;
+	printf("%d\n", a);
 }
 
 int main()
 {
 	TestClass();
-	//int a = 1, b = 2, c = 3;
-	//(a += b) += c;
-	//cout << a << endl;
 	return 0;
 }
